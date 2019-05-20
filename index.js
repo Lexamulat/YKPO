c54b272db1853ab451c5f055116cbdceec875504
daniil <daniil>
2019-05-20 22:42:57 +0300

    add brackets support

--- index.js
+++ index.js
@@ -1,5 +1,53 @@
+const openSymb = '(';
+const closeSymb = ')';
+
+
+function simple() {
+    return 'sml21'
+}
+
 function compile(str) {
+    while (~str.toString().indexOf(openSymb)) {
+        str = parse(str);
+    }
     return calc(str);
+}
+
+function parse(str) {
+    if (~(str.toString().indexOf(openSymb))) {
+        let obj = separateBlock(str);
+        let curr = `${obj.str1}${parse(obj.block)}${obj.str2}`;
+        console.log(curr);
+        return calc(curr);
+    }
+    return calc(str);
+
+}
+
+function separateBlock(str) {
+
+    let openCounter = 0;
+    let closeCounter = 0;
+    let obj = {};
+
+    for (let i = 0; i < str.length; i++) {
+        if (str[i] == openSymb) {
+            openCounter++;
+        }
+        else if (str[i] == closeSymb) {
+            closeCounter++;
+        }
+
+        if ((openCounter) && (openCounter == closeCounter)) {
+            let blockStart = str.indexOf(openSymb);
+            let blockEnd = i;
+            obj.str1 = str.substring(0, blockStart);
+            obj.block = str.substring(blockStart + 1, blockEnd);
+            obj.str2 = str.substring(blockEnd + 1);
+            return obj;
+        }
+    }
+
 }
 
 function calc(str) {
@@ -8,9 +56,10 @@ function calc(str) {
 
 
 
-// let str = '1+(4+9)*-(6+(-1))';
-let str='6-1*3';
+let str = '1+(4+9)*-(6+(-1))';
+// let str='(6+(-1))';
 
+console.log("eval", eval(str))
 try {
     console.log("compile", compile(str))
 } catch (err) {
