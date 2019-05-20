76c5b8690c74dd2f2155bf30de1fd1dd31916ece
daniil <daniil>
2019-05-20 23:08:02 +0300

    add test

--- test/index.js
+++ test/index.js
@@ -4,9 +4,13 @@ var expect = require('chai').expect;
 
 describe('#compile()', function () {
 
-    it('should return 0', function () {
+    it('-64', function () {
         expect(compile('1+(4+9)*-(6+(-1))')).to.equal(-64)
     })
 
+
+    it('zero call', function () {
+        expect(compile('0')).to.equal(0)
+    })
 
 })
